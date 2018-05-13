c08dcd1e19efd4b41add989228ea20343f0c0ac2
maobedkova <maryobedkova@gmail.com>
2018-05-13 17:28:51 +0000

    some version

--- HW10/nli_recodex_submission.py
+++ HW10/nli_recodex_submission.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+# coding=utf-8
+
+source_1 = """#!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
 
@@ -15,14 +17,14 @@ class Network:
     def construct(self, args, num_words, num_chars, num_languages):
         with self.session.graph.as_default():
             # Inputs
-            self.sentence_lens = tf.placeholder(tf.int32, [None], name="sentence_lens")
-            self.word_ids = tf.placeholder(tf.int32, [None, None], name="word_ids")
-            self.charseqs = tf.placeholder(tf.int32, [None, None], name="charseqs")
-            self.charseq_lens = tf.placeholder(tf.int32, [None], name="charseq_lens")
-            self.charseq_ids = tf.placeholder(tf.int32, [None, None], name="charseq_ids")
-            self.languages = tf.placeholder(tf.int32, [None], name="languages")
+            self.sentence_lens = tf.placeholder(tf.int32, [None], name=\"sentence_lens\")
+            self.word_ids = tf.placeholder(tf.int32, [None, None], name=\"word_ids\")
+            self.charseqs = tf.placeholder(tf.int32, [None, None], name=\"charseqs\")
+            self.charseq_lens = tf.placeholder(tf.int32, [None], name=\"charseq_lens\")
+            self.charseq_ids = tf.placeholder(tf.int32, [None, None], name=\"charseq_ids\")
+            self.languages = tf.placeholder(tf.int32, [None], name=\"languages\")
 
-            self.is_training = tf.placeholder(tf.bool, [], name="is_training")
+            self.is_training = tf.placeholder(tf.bool, [], name=\"is_training\")
 
             # Training.
             # Define:
@@ -30,26 +32,26 @@ class Network:
             # - training in `self.training`
             # - predictions in `self.predictions`
 
-            word_embeddings = tf.get_variable("word_embeddings", [num_words, 64])
+            word_embeddings = tf.get_variable(\"word_embeddings\", [num_words, 64])
             embedded_word_ids = tf.nn.embedding_lookup(word_embeddings, self.word_ids)
 
             feats = []
             for kernel_size in range(2, 6):
                 hidden_layer = tf.layers.conv1d(embedded_word_ids, filters=24,
-                                                kernel_size=kernel_size, strides=1, padding="valid")
-                #hidden_layer = tf.layers.dropout(hidden_layer, rate=0.3, training=self.is_training, name="dropout")
-                feats.append(tf.layers.max_pooling1d(hidden_layer, 500, strides=1, padding="same")[:, 1, :])
+                                                kernel_size=kernel_size, strides=1, padding=\"valid\")
+                #hidden_layer = tf.layers.dropout(hidden_layer, rate=0.3, training=self.is_training, name=\"dropout\")
+                feats.append(tf.layers.max_pooling1d(hidden_layer, 500, strides=1, padding=\"same\")[:, 1, :])
 
             concat_feats = tf.concat(feats, axis=-1)
 
             output_layer = tf.layers.dense(concat_feats, units=num_languages, activation=None)
-            #output_layer = tf.layers.dropout(output_layer, rate=0.3, training=self.is_training, name="dropout")
+            #output_layer = tf.layers.dropout(output_layer, rate=0.3, training=self.is_training, name=\"dropout\")
 
             self.predictions = tf.argmax(output_layer, axis=-1)
 
             loss = tf.losses.sparse_softmax_cross_entropy(self.languages, output_layer)
             global_step = tf.train.create_global_step()
-            self.training = tf.train.AdamOptimizer().minimize(loss, global_step=global_step, name="training")
+            self.training = tf.train.AdamOptimizer().minimize(loss, global_step=global_step, name=\"training\")
 
             # Summaries
             self.current_accuracy, self.update_accuracy = tf.metrics.accuracy(self.languages, self.predictions)
@@ -59,12 +61,12 @@ class Network:
             summary_writer = tf.contrib.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
             self.summaries = {}
             with summary_writer.as_default(), tf.contrib.summary.record_summaries_every_n_global_steps(100):
-                self.summaries["train"] = [tf.contrib.summary.scalar("train/loss", self.update_loss),
-                                           tf.contrib.summary.scalar("train/accuracy", self.update_accuracy)]
+                self.summaries[\"train\"] = [tf.contrib.summary.scalar(\"train/loss\", self.update_loss),
+                                           tf.contrib.summary.scalar(\"train/accuracy\", self.update_accuracy)]
             with summary_writer.as_default(), tf.contrib.summary.always_record_summaries():
-                for dataset in ["dev", "test"]:
-                    self.summaries[dataset] = [tf.contrib.summary.scalar(dataset + "/loss", self.current_loss),
-                                               tf.contrib.summary.scalar(dataset + "/accuracy", self.current_accuracy)]
+                for dataset in [\"dev\", \"test\"]:
+                    self.summaries[dataset] = [tf.contrib.summary.scalar(dataset + \"/loss\", self.current_loss),
+                                               tf.contrib.summary.scalar(dataset + \"/accuracy\", self.current_accuracy)]
 
             # Initialize variables
             self.session.run(tf.global_variables_initializer())
@@ -73,10 +75,10 @@ class Network:
 
     def train_epoch(self, train, batch_size):
         while not train.epoch_finished():
-            sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens, tags, levels, prompts, languages = \
+            sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens, tags, levels, prompts, languages = \\
                 train.next_batch(batch_size)
             self.session.run(self.reset_metrics)
-            self.session.run([self.training, self.summaries["train"]],
+            self.session.run([self.training, self.summaries[\"train\"]],
                              {self.sentence_lens: sentence_lens,
                               self.charseqs: charseqs, self.charseq_lens: charseq_lens,
                               self.word_ids: word_ids, self.charseq_ids: charseq_ids,
@@ -86,7 +88,7 @@ class Network:
     def evaluate(self, dataset_name, dataset, batch_size):
         self.session.run(self.reset_metrics)
         while not dataset.epoch_finished():
-            sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens, tags, levels, prompts, languages = \
+            sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens, tags, levels, prompts, languages = \\
                 dataset.next_batch(batch_size)
             self.session.run([self.update_accuracy, self.update_loss],
                              {self.sentence_lens: sentence_lens,
@@ -100,7 +102,7 @@ class Network:
     def predict(self, dataset, batch_size):
         languages = []
         while not dataset.epoch_finished():
-            sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens, tags, levels, prompts, _ = \
+            sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens, tags, levels, prompts, _ = \\
                 dataset.next_batch(batch_size)
             languages.extend(self.session.run(self.predictions,
                                               {self.sentence_lens: sentence_lens,
@@ -111,7 +113,7 @@ class Network:
         return languages
 
 
-if __name__ == "__main__":
+if __name__ == \"__main__\":
     import argparse
     import datetime
     import os
@@ -122,37 +124,50 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=24, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=30, type=int, help="Number of epochs.")
-    parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
+    parser.add_argument(\"--batch_size\", default=24, type=int, help=\"Batch size.\")
+    parser.add_argument(\"--epochs\", default=30, type=int, help=\"Number of epochs.\")
+    parser.add_argument(\"--threads\", default=4, type=int, help=\"Maximum number of threads to use.\")
     args = parser.parse_args()
 
     # Create logdir name
-    args.logdir = "logs/{}-{}-{}".format(
+    args.logdir = \"logs/{}-{}-{}\".format(
         os.path.basename(__file__),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+        datetime.datetime.now().strftime(\"%Y-%m-%d_%H%M%S\"),
+        \",\".join((\"{}={}\".format(re.sub(\"(.)[^_]*_?\", r\"\\1\", key), value) for key, value in sorted(vars(args).items())))
     )
-    if not os.path.exists("logs"): os.mkdir("logs") # TF 1.6 will do this by itself
+    if not os.path.exists(\"logs\"): os.mkdir(\"logs\") # TF 1.6 will do this by itself
 
     # Load the data
-    train = nli_dataset.NLIDataset("nli-train.txt")
-    dev = nli_dataset.NLIDataset("nli-dev.txt", train=train, shuffle_batches=False)
-    test = nli_dataset.NLIDataset("nli-test.txt", train=train, shuffle_batches=False)
+    train = nli_dataset.NLIDataset(\"nli-train.txt\")
+    dev = nli_dataset.NLIDataset(\"nli-dev.txt\", train=train, shuffle_batches=False)
+    test = nli_dataset.NLIDataset(\"nli-test.txt\", train=train, shuffle_batches=False)
 
     # Construct the network
     network = Network(threads=args.threads)
-    network.construct(args, len(train.vocabulary("words")), len(train.vocabulary("chars")), len(train.vocabulary("languages")))
+    network.construct(args, len(train.vocabulary(\"words\")), len(train.vocabulary(\"chars\")), len(train.vocabulary(\"languages\")))
 
     # Train
     for i in range(args.epochs):
         network.train_epoch(train, args.batch_size)
 
-        acc = network.evaluate("dev", dev, args.batch_size)
-        print(i, "{:.2f}".format(100 * acc))
+        acc = network.evaluate(\"dev\", dev, args.batch_size)
+        print(i, \"{:.2f}\".format(100 * acc))
 
     # Predict test data
-    with open("{}/nli_test.txt".format(args.logdir), "w", encoding="utf-8") as test_file:
+    with open(\"{}/nli_test.txt\".format(args.logdir), \"w\", encoding=\"utf-8\") as test_file:
         languages = network.predict(test, args.batch_size)
         for language in languages:
-            print(test.vocabulary("languages")[language], file=test_file)
+            print(test.vocabulary(\"languages\")[language], file=test_file)
+"""
+
+test_data = b'{Wp48S^xk9=GL@E000009N$DCAGY&-0RR9100dcD'
+
+if __name__ == "__main__":
+    import base64
+    import io
+    import lzma
+    import sys
+
+    with io.BytesIO(base64.b85decode(test_data)) as lzma_data:
+        with lzma.open(lzma_data, "r") as lzma_file:
+            sys.stdout.buffer.write(lzma_file.read())
